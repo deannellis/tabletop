133793d0e0adda06ddcc9d6cda0935615b8b4b1b
Nellis,Dean <dean.nellis@lexmark.com>
2020-11-28 00:56:48 -0500

    additional styling to forms

--- imports/ui/components/CharacterGenerator/CharacterDescription/index.jsx
+++ imports/ui/components/CharacterGenerator/CharacterDescription/index.jsx
@@ -89,135 +89,140 @@ const CharacterDescription = ({ onSubmitDescription, knownLanguages }) => {
   return (
     <div>
       <h1 className="header--underlined">Describe Your Character</h1>
+      <div className="character-generator__form-wrapper">
+        <div className="character-generator__form">
+          <h2>Choose your alignment</h2>
+          <div className="input-group">
+            <select
+              value={alignment}
+              className="select-input"
+              onChange={(e) => {
+                setAlignment(e.target.value);
+              }}
+            >
+              {alignmentShorthands.map((alignment) => (
+                <option value={alignment}>{printAlignment(alignment)}</option>
+              ))}
+              <option value=""></option>
+            </select>
+            <label className="label">Alignment</label>
+          </div>
+          <h2>Describe your background</h2>
+          <div className="input-group">
+            <div className="input__error">
+              {errorIndexes.includes(0) ? "Background Title is Required" : ""}
+            </div>
+            <input
+              type="text"
+              className={`text-input ${
+                errorIndexes.includes(0) ? "input--error" : ""
+              }`}
+              value={bgTitle}
+              onChange={(e) => {
+                setBGTitle(e.target.value);
+              }}
+            />
+            <label className="label">Background Title</label>
+          </div>
+          <div className="input-group">
+            <div className="input__error">
+              {errorIndexes.includes(1) ? "Personality Trait is Required" : ""}
+            </div>
+            <textarea
+              className={`text-area ${
+                errorIndexes.includes(0) ? "input--error" : ""
+              }`}
+              value={personalityTrait}
+              onChange={(e) => {
+                setPersonalityTrait(e.target.value);
+              }}
+            ></textarea>
+            <label className="label">Personality Trait</label>
+          </div>
+          <div className="input-group">
+            <div className="input__error">
+              {errorIndexes.includes(2) ? "Ideal is Required" : ""}
+            </div>
+            <textarea
+              value={ideal}
+              className={`text-area ${
+                errorIndexes.includes(0) ? "input--error" : ""
+              }`}
+              onChange={(e) => {
+                setIdeal(e.target.value);
+              }}
+            ></textarea>
+            <label className="label">Ideal</label>
+          </div>
 
-      <h2>Choose your alignment</h2>
-      <div className="input-group">
-        <select
-          value={alignment}
-          className="select-input"
-          onChange={(e) => {
-            setAlignment(e.target.value);
-          }}
-        >
-          {alignmentShorthands.map((alignment) => (
-            <option value={alignment}>{printAlignment(alignment)}</option>
-          ))}
-          <option value=""></option>
-        </select>
-        <label className="label">Alignment</label>
-      </div>
-      <h2>Describe your background</h2>
-      <div className="input-group">
-        <div className="input__error">
-          {errorIndexes.includes(0) ? "Background Title is Required" : ""}
-        </div>
-        <input
-          type="text"
-          className={`text-input ${
-            errorIndexes.includes(0) ? "input--error" : ""
-          }`}
-          value={bgTitle}
-          onChange={(e) => {
-            setBGTitle(e.target.value);
-          }}
-        />
-        <label className="label">Background Title</label>
-      </div>
-      <div className="input-group">
-        <div className="input__error">
-          {errorIndexes.includes(1) ? "Personality Trait is Required" : ""}
-        </div>
-        <textarea
-          className={`text-area ${
-            errorIndexes.includes(0) ? "input--error" : ""
-          }`}
-          value={personalityTrait}
-          onChange={(e) => {
-            setPersonalityTrait(e.target.value);
-          }}
-        ></textarea>
-        <label className="label">Personality Trait</label>
-      </div>
-      <div className="input-group">
-        <div className="input__error">
-          {errorIndexes.includes(2) ? "Ideal is Required" : ""}
-        </div>
-        <textarea
-          value={ideal}
-          className={`text-area ${
-            errorIndexes.includes(0) ? "input--error" : ""
-          }`}
-          onChange={(e) => {
-            setIdeal(e.target.value);
-          }}
-        ></textarea>
-        <label className="label">Ideal</label>
-      </div>
+          <div className="input-group">
+            <div className="input__error">
+              {errorIndexes.includes(3) ? "Bond is Required" : ""}
+            </div>
+            <textarea
+              value={bond}
+              className={`text-area ${
+                errorIndexes.includes(0) ? "input--error" : ""
+              }`}
+              onChange={(e) => {
+                setBond(e.target.value);
+              }}
+            ></textarea>
+            <label className="label">Bond</label>
+          </div>
+          <div className="input-group">
+            <div className="input__error">
+              {errorIndexes.includes(4) ? "Ideal is Required" : ""}
+            </div>
+            <textarea
+              value={flaw}
+              className={`text-area ${
+                errorIndexes.includes(0) ? "input--error" : ""
+              }`}
+              onChange={(e) => {
+                setFlaw(e.target.value);
+              }}
+            ></textarea>
+            <label className="label">Flaw</label>
+          </div>
+          <h2>Choose 2 Skill Proficiencies</h2>
 
-      <div className="input-group">
-        <div className="input__error">
-          {errorIndexes.includes(3) ? "Bond is Required" : ""}
+          <SkillSelect
+            bgProficiency={bgProficiency1}
+            setBGProficiency={setBGProficiency1}
+          />
+          <div className="space-l"></div>
+          <SkillSelect
+            bgProficiency={bgProficiency2}
+            setBGProficiency={setBGProficiency2}
+          />
+          <div className="input__error">
+            {bgProficiency1 === bgProficiency2
+              ? "Choose 2 separate skills"
+              : ""}
+          </div>
+          <h2>Choose 2 additional Perks</h2>
+          <PerkSelect
+            knownLanguages={knownLanguages}
+            bgPerk={bgPerk1}
+            setBGPerk={setBGPerk1}
+          />
+          <PerkSelect
+            knownLanguages={knownLanguages}
+            bgPerk={bgPerk2}
+            setBGPerk={setBGPerk2}
+          />
+          <div className="input__error">
+            {bgPerk1 === bgPerk2 && bgPerk1 !== undefined && bgPerk2 !== ""
+              ? "Choose 2 separate skills. "
+              : ""}
+            {errorIndexes.includes(5) ? " Perks are Required" : ""}
+          </div>
+          <button className="button" onClick={handleSubmit}>
+            Submit Description
+          </button>
         </div>
-        <textarea
-          value={bond}
-          className={`text-area ${
-            errorIndexes.includes(0) ? "input--error" : ""
-          }`}
-          onChange={(e) => {
-            setBond(e.target.value);
-          }}
-        ></textarea>
-        <label className="label">Bond</label>
       </div>
-      <div className="input-group">
-        <div className="input__error">
-          {errorIndexes.includes(4) ? "Ideal is Required" : ""}
-        </div>
-        <textarea
-          value={flaw}
-          className={`text-area ${
-            errorIndexes.includes(0) ? "input--error" : ""
-          }`}
-          onChange={(e) => {
-            setFlaw(e.target.value);
-          }}
-        ></textarea>
-        <label className="label">Flaw</label>
-      </div>
-      <h2>Choose 2 Skill Proficiencies</h2>
-
-      <SkillSelect
-        bgProficiency={bgProficiency1}
-        setBGProficiency={setBGProficiency1}
-      />
-      <div className="space-l"></div>
-      <SkillSelect
-        bgProficiency={bgProficiency2}
-        setBGProficiency={setBGProficiency2}
-      />
-      <div className="input__error">
-        {bgProficiency1 === bgProficiency2 ? "Choose 2 separate skills" : ""}
-      </div>
-      <h2>Choose 2 additional Perks</h2>
-      <PerkSelect
-        knownLanguages={knownLanguages}
-        bgPerk={bgPerk1}
-        setBGPerk={setBGPerk1}
-      />
-      <PerkSelect
-        knownLanguages={knownLanguages}
-        bgPerk={bgPerk2}
-        setBGPerk={setBGPerk2}
-      />
-      <div className="input__error">
-        {bgPerk1 === bgPerk2 && bgPerk1 !== undefined && bgPerk2 !== ""
-          ? "Choose 2 separate skills. "
-          : ""}
-        {errorIndexes.includes(5) ? " Perks are Required" : ""}
-      </div>
-      <button className="button" onClick={handleSubmit}>
-        Submit Description
-      </button>
     </div>
   );
 };
--- imports/ui/components/CharacterGenerator/Finalize.jsx
+++ imports/ui/components/CharacterGenerator/Finalize.jsx
@@ -6,32 +6,36 @@ const Finalize = ({ onFinalize }) => {
   return (
     <div>
       <h1 className="header--underlined">Finalize Character</h1>
-      <h2>Name your character</h2>
-      <div className="input-group">
-        <div className="input__error">{!!error ? error : " "}</div>
-        <input
-          type="text"
-          className={`text-input ${!!error && "input--error"}`}
-          value={charName}
-          onChange={(e) => {
-            setCharName(e.target.value);
-          }}
-        />
+      <div className="character-generator__form-wrapper">
+        <div className="character-generator__form">
+          <h2>Name your character</h2>
+          <div className="input-group">
+            <div className="input__error">{!!error ? error : " "}</div>
+            <input
+              type="text"
+              className={`text-input ${!!error && "input--error"}`}
+              value={charName}
+              onChange={(e) => {
+                setCharName(e.target.value);
+              }}
+            />
 
-        <label>Name </label>
+            <label>Name </label>
+          </div>
+          <button
+            className="button"
+            onClick={() => {
+              if (charName.length === 0) {
+                setError("Name is required");
+                return;
+              }
+              onFinalize(charName);
+            }}
+          >
+            Finish Character
+          </button>
+        </div>
       </div>
-      <button
-        className="button"
-        onClick={() => {
-          if (charName.length === 0) {
-            setError("Name is required");
-            return;
-          }
-          onFinalize(charName);
-        }}
-      >
-        Finish Character
-      </button>
     </div>
   );
 };
